48c5a1b9259836ceb5b22769070caba96ac0db08
ADudeSomewhere <89604794+ADudeSomewhere@users.noreply.github.com>
2021-10-25 09:18:04 -0500

    Update HangmanManager.java

--- HangmanManager.java
+++ HangmanManager.java
@@ -5,7 +5,7 @@ public class HangmanManager
 	private static HashSet<Character> letter; //for guesses
 	private static String pattern;// for pattern
 	private static HashMap<String, HashSet<String>> temp;//for record
-	private static Hashset<String> Richard;// for dictionary
+	private static HashSet<String> Richard;// for dictionary
 	public HangmanManager( List<String> dictionary, int length, int max )
 	{
 		letter = new HashSet<Character>();

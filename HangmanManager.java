e55a4701ddcf50b22b5a5aea2d2866020099cf3c
drjkfseosilfsdkljsdogijdgjkd <92867176+drjkfseosilfsdkljsdogijdgjkd@users.noreply.github.com>
2021-10-27 09:16:08 -0500

    Update HangmanManager.java

--- HangmanManager.java
+++ HangmanManager.java
@@ -43,33 +43,50 @@ public class HangmanManager
 	public int record( char guess )
 	{
 		if(maxguesses-wrongguessesmade<1||Richard.size()==0) {
-		if(!letter.contains(guess)) {
-		ArrayList<String> Carl= (ArrayList<String>)Richard.stream().collect(Collectors.toList());
-		letter.add(guess);
-		for(int x = 0; x<Carl.size(); x++){
-			String m = String.valueOf(guess);
-			String t = Carl.get(x);
-			for(int y = 0; y<t.length();y++){
-				if(!t.substring(y, y+1).equals(m) && y!=t.length-1)
-					t= t.substring(0, y) + "_" + t.substring(y+1);
-				else if(!t.substring(y, y+1).equals(m))
-					t = t.substring(0, y) + "_";
+			if(!letter.contains(guess)) {
+				temp = new HashMap<String, ArrayList<String>>;
+				ArrayList<String> Carl= (ArrayList<String>)Richard.stream().collect(Collectors.toList());//converts Richard
+				letter.add(guess); // adds a gues to letter
+				for(int x = 0; x<Carl.size(); x++){ //goes through Carl to sort all options
+					String m = String.valueOf(guess);
+					String t = Carl.get(x);
+					for(int y = 0; y<t.length();y++){ //creates key for each option
+						if(!t.substring(y, y+1).equals(m) && y!=t.length()-1)
+							t= t.substring(0, y) + "_" + t.substring(y+1);
+						else if(!t.substring(y, y+1).equals(m))
+							t = t.substring(0, y) + "_";
+					}	
+					if(temp.get(t)==null) { //adds option to family in temp
+						ArrayList<String> temper = new ArrayList<String>();
+						temper.add(Carl.get(x));
+						temp.put(t, temper);
+					}
+					else{
+						ArrayList<String> ab = temp.get(t);
+						ab.add(Carl.get(x));
+						temp.put(t, ab);
+					}
+				}
+				ArrayList<Integer> fLength = new ArrayList<Integer>();
+				for(String s : temp.entrySet()){ //goes through map and finds biggest family
+					int cnt = map.get(s).size();
+					fLength.add(cnt);
+				}
+				int pos = -1;//place holder
+				int min = Integer.MIN_VALUE;
+				for(int x = 0; x<fLength.size(); x++){
+					if(fLength.get(x)>min){
+						pos = x;
+						min = fLength.get(x);
+					}
+				}
+				String zzz = temp.entrySet().get(pos);//CHANGE THIS!!!!!!
+				Richard = new HashSet<String>();
+				for(int x = 0; x<map.get(zzz).size(); x++){
+					Richard.add(map.get(zzz).get(x));
+				}
 			}
-			if(temp.get(t)==null)
-				temp.put(t, new List<String>);
-			else{
-				ArrayList<String> ab = temp.get(t);
-				ab.add(Carl.get(x));
-				temp.put(t, ab);
-			}
-		}
-		ArrayList<Integer> fLength = new ArrayList<Integer>();
-		while(temp.hasKey()){
-			int cnt = 0;
-			for(int x = 0; x<
-		}
-		}
-		else {throw new IllegalArgumentException("letter has already been guessed");}
+			else {throw new IllegalArgumentException("letter has already been guessed");}
 		}
 		else{throw new IllegalStateException("out of guesses/out of options");}
 	}// increment wrongguessesmade 1 higher for every wrong guess 
